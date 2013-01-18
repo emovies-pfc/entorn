b370b912cea080e0857729ff5855797bb1e81bdc
Roger Llopart Pla <lumbendil@gmail.com>
2013-01-18 21:11:58 +0100

    Entorn utilitzable per a una instalació de Symfony2.

---
+++ cookbooks/vagrant_main/attributes/default.rb
@@ -0,0 +1,1 @@
+default['vagrant_main']['php']['apache_conf_dir'] = "/etc/php5/apache2"
--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,4 +1,16 @@
 include_recipe "apt"
+
+apt_repository "php54" do
+  uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
+  distribution "precise"
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "E5267A6C"
+  action :add
+  notifies :run, "execute[apt-get update]", :immediately
+  deb_src true
+end
+
 include_recipe "apache2"
 include_recipe "mysql"
 include_recipe "mysql::server"
@@ -12,6 +24,18 @@ package "php5-intl" do
 	action :install
 end
 
+package "php-apc" do
+	action :install
+end
+
+package "vim" do
+	action :install
+end
+
+package "php5-xdebug" do
+	action :install
+end
+
 execute "disable-default-site" do
   command "sudo a2dissite default"
   notifies :reload, resources(:service => "apache2"), :delayed
@@ -19,5 +43,14 @@ end
 
 web_app "project" do
   template "project.conf.erb"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
+
+template "#{node['vagrant_main']['php']['apache_conf_dir']}/php.ini" do
+  source "php.ini.erb"
+  cookbook "php"
+  owner "root"
+  group "root"
+  mode "0644"
   notifies :reload, resources(:service => "apache2"), :delayed
 end
