fe13a1bc01e0a491fc8784a91253be0162818929
Roger Llopart Pla <lumbendil@gmail.com>
2013-05-20 17:18:27 +0200

    Added the Java gearman.

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,25 +1,52 @@
 include_recipe "apt"
 
 apt_repository "php54" do
-  uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
-  distribution "precise"
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "E5267A6C"
-  action :add
-  notifies :run, "execute[apt-get update]", :immediately
-  deb_src true
+	uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
+	distribution "precise"
+	components ["main"]
+	keyserver "keyserver.ubuntu.com"
+	key "E5267A6C"
+	action :add
+	notifies :run, "execute[apt-get update]", :immediately
+	deb_src true
+end
+
+apt_repository "gearman" do
+	uri "http://ppa.launchpad.net/gearman-developers/ppa/ubuntu"
+	distribution "precise"
+	components ["main"]
+	keyserver "keyserver.ubuntu.com"
+	key "1C73E014"
+	action :add
+	notifies :run, "execute[apt-get update]", :immediately
+	deb_src true
 end
 
 include_recipe "apache2"
 include_recipe "mysql"
 include_recipe "mysql::server"
 include_recipe "apache2::mod_php5"
+include_recipe "php"
 include_recipe "git"
 include_recipe "memcached"
+include_recipe "java"
+include_recipe "python"
+include_recipe "supervisor"
 
 package "curl" do
     action :install
+end
+
+package "gearman" do
+	action :install
+end
+
+package "php-pear" do
+	action :install
+end
+
+package "libgearman-dev" do
+	action :install
 end
 
 package "php5-mysql" do
@@ -54,13 +81,22 @@ package "acl" do
 	action :install
 end
 
-template "#{node['vagrant_main']['php']['apache_conf_dir']}/php.ini" do
-  source "php.ini.erb"
-  cookbook "php"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :reload, resources(:service => "apache2"), :delayed
+package "make" do
+	action :install
+end
+
+php_pear "gearman" do
+	action :install
+end
+
+file "#{node['vagrant_main']['php']['apache_conf_dir']}/php.ini" do
+	action :delete
+end
+
+link "#{node['vagrant_main']['php']['apache_conf_dir']}/php.ini" do
+	action :create
+	to "#{node['php']['conf_dir']}/php.ini"
+	notifies :reload, resources(:service => "apache2"), :delayed
 end
 
 if File.exists?("/usr/local/bin/composer")
@@ -111,4 +147,8 @@ end
 
 execute "create-database" do
   command "mysql -u root -p#{node[:mysql][:server_root_password]} -e \"CREATE DATABASE IF NOT EXISTS emovie\""
+end
+
+supervisor_service "recommender" do
+	command "java -jar /vagrant/recomender-1.0-SNAPSHOT-jar-with-dependencies.jar"
 end
